c5bdf0609d5aac76d9f855e1e951b96d918b781d
avdeysa <45868091+avdeysa@users.noreply.github.com>
2018-12-14 12:08:41 +0300

    lol
    123123

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("lol,oll");
+            Console.WriteLine("lol,oll");
         }
     }
 }
