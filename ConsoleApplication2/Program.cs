420bfeb8846efb18f3c049e2375133614dbb3756
avdeysa <45868091+avdeysa@users.noreply.github.com>
2018-12-14 12:12:29 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("lolololololololooooooooooooooooooooooooooooooooooooooooooo");
+            Console.WriteLine("lolooooooooooooooooooo");
             Console.WriteLine("lol,oll");
             
         }
